3455977ed5e4a00bbcbf9b3803e4182edd45d7e5
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2024-12-17 23:46:57 +0800

    Update bilibili.py

--- src/bilibili.py
+++ src/bilibili.py
@@ -48,7 +48,7 @@ class QtCore(QMainWindow, Ui_MainWindow):
 		vids, groups = {}, {}
 		for i in range(self.treeWidget_old.topLevelItemCount()):
 			item = self.treeWidget_old.topLevelItem(i)
-			if item.data(0, Qt.ItemDataRole.UserRole):
+			if item.childCount():
 				for j in range(item.childCount()):
 					if item.child(j).checkState(0) == Qt.CheckState.Checked:
 						vids[item.child(j).toolTip(0).split("/")[-1]] = util.BANNED_CHAR.sub("_", item.child(j).text(0))
@@ -97,7 +97,6 @@ class QtCore(QMainWindow, Ui_MainWindow):
 			jsn_path = os.path.join(path_old, folder_name, "videoInfo.json").replace("\\", "/")
 			if not os.path.exists(jsn_path):
 				continue
-
 			jsn = util.FileIO.read(jsn_path)
 			groups.setdefault(jsn["groupTitle"], []).append(folder_name)
 			titles[folder_name] = jsn["title"]
@@ -105,28 +104,21 @@ class QtCore(QMainWindow, Ui_MainWindow):
 		for group, folder_names in groups.items():
 			parent = None
 			if len(folder_names) != 1:
-				parent = QTreeWidgetItem([group])
+				parent = QTreeWidgetItem([f"0{group}"])
 				parent.setCheckState(0, Qt.CheckState.Unchecked)
 				parent.setIcon(0, util.icon("folder"))
-				parent.setData(0, Qt.ItemDataRole.UserRole, True)
 				self.treeWidget_old.addTopLevelItem(parent)
-
 			for folder_name in folder_names:
 				child = QTreeWidgetItem([titles[folder_name]])
 				child.setCheckState(0, Qt.CheckState.Unchecked)
 				child.setIcon(0, util.icon("video" if self.radioButton_mp4.isChecked() else "audio"))
-				child.setData(0, Qt.ItemDataRole.UserRole, False)
 				child.setToolTip(0, os.path.join(path_old, folder_name).replace("\\", "/"))
-				if len(folder_names) != 1:
+				if parent:
 					parent.addChild(child)
 				else:
 					self.treeWidget_old.addTopLevelItem(child)
-			if len(folder_names) != 1:
-				parent.sortChildren(0, Qt.SortOrder.AscendingOrder)
+					child.setText(0, f"1{child.text(0)}")
 
-		for i in range(self.treeWidget_old.topLevelItemCount()):
-			item = self.treeWidget_old.topLevelItem(i)
-			item.setText(0, f"{int(not item.data(0, Qt.ItemDataRole.UserRole))}{item.text(0)}")
 		self.treeWidget_old.sortItems(0, Qt.SortOrder.AscendingOrder)
 		for i in range(self.treeWidget_old.topLevelItemCount()):
 			item = self.treeWidget_old.topLevelItem(i)
