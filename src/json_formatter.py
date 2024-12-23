6936e7f6a7893ac491af58f60852c47eef376052
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2024-12-24 00:04:35 +0800

    Update json_formatter.py

--- src/json_formatter.py
+++ src/json_formatter.py
@@ -5,11 +5,9 @@ import json
 import sys
 import util
 
-COLOR = {
-	"red": "rgb(163,21,21)",
-	"blue": "rgb(4,81,165)",
-	"green": "rgb(9,134,88)"
-}
+COLOR_RED = "rgb(163,21,21)"
+COLOR_BLUE = "rgb(4,81,165)"
+COLOR_GREEN = "rgb(9,134,88)"
 
 
 class QtCore(QMainWindow, Ui_MainWindow):
@@ -21,8 +19,11 @@ class QtCore(QMainWindow, Ui_MainWindow):
 	def keyPressEvent(self, a0):
 		if a0.key() != Qt.Key.Key_F4:
 			return
+
 		try:
 			jsn = json.loads(self.textEdit.toPlainText())
+			if isinstance(jsn, dict):
+				jsn = dict(sorted(jsn.items()))
 		except:
 			return self.textEdit.setHtml(self.textEdit.toPlainText().replace("\n", "<br>").replace(" ", "&nbsp;"))
 
@@ -38,19 +39,19 @@ class QtCore(QMainWindow, Ui_MainWindow):
 			if line[-3:] in (": {", ": [") or line[-4:] in (": {}", ": []"):
 				pos = line.rfind(":")
 				text = line[:pos].replace(" ", "&nbsp;")
-				htmls.append(html % (COLOR["red"], text, line[pos:] + comma))
+				htmls.append(html % (COLOR_RED, text, line[pos:] + comma))
 				continue
 			if '"' not in line or (line.count('"') == 2 and '"' not in line.strip()[1:-1]):
 				text = line.replace(" ", "&nbsp;")
-				color = COLOR["blue" if '"' in line else "green"]
+				color = COLOR_BLUE if '"' in line else COLOR_GREEN
 				htmls.append(html % (color, text, comma))
 				continue
 
 			pos = line.strip()[1:].find('"') + (len(line) - len(line.strip()) + 2)
-			color = COLOR["blue" if line.endswith('"') else "green"]
+			color = COLOR_BLUE if line.endswith('"') else COLOR_GREEN
 			text_key = line[:pos].replace(" ", "&nbsp;")
 			text_value = line[pos + 2:].replace(" ", "&nbsp;")
-			htmls.append(html % (COLOR["red"], text_key, ": ") + html % (color, text_value, comma))
+			htmls.append(html % (COLOR_RED, text_key, ": ") + html % (color, text_value, comma))
 		self.textEdit.setHtml("<br>".join(htmls))
 
 
