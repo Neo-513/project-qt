9e42232a932ba94e6876dfb5920c93e7a22f70ae
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-02-23 03:39:18 +0800

    Create wordle_pre.py

---
+++ precomputation/wordle_pre.py
@@ -0,0 +1,73 @@
+from src.wordle import *
+from collections import Counter
+from multiprocessing import cpu_count, Pool
+
+PATH.update({"compose": util.join_path(util.RESOURCE, "wordle", "cache_compose.pkl")})
+CACHE.update({"compose": util.FileIO.read(PATH["compose"]) if os.path.exists(PATH["compose"]) else None})
+
+
+def compute_index():
+	cache_index = {word: i for i, word in enumerate(ALLOWED_WORDS)}
+	util.FileIO.write(PATH["index"], cache_index)
+
+
+def compute_compose():
+	cache_compose = {word: dict(Counter(word)) for word in ALLOWED_WORDS}
+	util.FileIO.write(PATH["compose"], cache_compose)
+
+
+def compute_state():
+	cache_state = np.zeros(TOTALITY ** 2, dtype=np.uint8)
+	for (i, guess), (j, answer) in product(enumerate(ALLOWED_WORDS), repeat=2):
+		cache_state[i * TOTALITY + j] = __compute_state(guess, answer)
+	cache_state.tofile(PATH["state"])
+
+
+def __compute_state(guess, answer):
+	state, compose, uncertain = ["0"] * 5, CACHE["compose"][answer].copy(), []
+	for i, g in enumerate(guess):
+		if g == answer[i]:
+			state[i] = "2"
+			compose[g] -= 1
+		elif g in answer:
+			uncertain.append(i)
+	for i in uncertain:
+		if compose[guess[i]]:
+			state[i] = "1"
+			compose[guess[i]] -= 1
+	return int("".join(state), 3)
+
+
+
+
+
+import mylibrary.myutil as mu
+
+
+
+
+@mu.Decorator.timing
+def compute_worst():
+	tasks = (guess for guess in ALLOWED_WORDS)
+	with Pool(processes=cpu_count()) as pool:
+		cache_worst = dict(pool.imap_unordered(__compute_worst, tasks))
+	util.FileIO.write(PATH["worst"], cache_worst)
+
+
+def __compute_worst(guess):
+	candidate = MyComputation.to_candidate(guess, 0, ALLOWED_WORDS)
+	best_guess = EntropyAlgorithm.infer(candidate)
+	print(guess, len(candidate))#, best_guess)
+	return guess, best_guess
+
+
+
+
+
+if __name__ == "__main__":
+	# compute_index()
+	# compute_compose()
+	# compute_state()
+	#compute_worst()
+
+	print(CACHE["state"])
