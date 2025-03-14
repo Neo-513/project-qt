24927bbad568f3c690ba942cf6e974106b5f14b4
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-03-15 01:42:10 +0800

    Create sudoku_cache.py

---
+++ cache/sudoku/sudoku_cache.py
@@ -0,0 +1,14 @@
+from src.sudoku import *
+
+
+def compute_graph():
+	graph = np.zeros((729, 324), dtype=np.uint8)
+	for i, j, k in product(range(9), repeat=3):
+		g = graph[i * 81 + j * 9 + k]
+		g[i * 9 + j] = g[81 + i * 9 + k] = g[162 + j * 9 + k] = g[243 + i // 3 * 27 + j // 3 * 9 + k] = 1
+	graph.tofile(PATH["graph"])
+
+
+if __name__ == "__main__":
+	# compute_graph()
+	pass
