5e8dabfccaf6585c3099385b1ba855a0c3a9c875
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-03-18 02:20:33 +0800

    Update wordle_cache.py

--- cache/wordle_cache.py
+++ cache/wordle_cache.py
@@ -4,27 +4,27 @@ from multiprocessing import cpu_count, Pool
 
 
 def compute_index():
-	cache_index = {word: i for i, word in enumerate(ALLOWED_WORDS)}
-	util.write(PATH["index"], cache_index)
+	cache = {word: i for i, word in enumerate(ALLOWED_WORDS)}
+	util.write(PATH["index"], cache)
 
 
 def compute_compose():
-	cache_compose = {word: dict(Counter(word)) for word in ALLOWED_WORDS}
-	util.write(PATH["compose"], cache_compose)
+	cache = {word: dict(Counter(word)) for word in ALLOWED_WORDS}
+	util.write(PATH["compose"], cache)
 
 
 def compute_state():
-	cache_state = np.zeros(MyComputation.TOTALITY ** 2, dtype=np.uint8)
+	cache = np.zeros(MyComputation.TOTALITY ** 2, dtype=np.uint8)
 	for (i, guess), (j, answer) in product(enumerate(ALLOWED_WORDS), repeat=2):
-		cache_state[i * MyComputation.TOTALITY + j] = __compute_state(guess, answer)
-	cache_state.tofile(PATH["state"])
+		cache[i * MyComputation.TOTALITY + j] = __compute_state(guess, answer)
+	cache.tofile(PATH["state"])
 
 
 def compute_worst(worst):
 	tasks = ((guess, int(worst, 3)) for guess in ALLOWED_WORDS)
 	with Pool(processes=cpu_count()) as pool:
-		cache_worst = dict(pool.imap_unordered(__compute_worst, tasks))
-	util.write(PATH[f"worst_{worst}"], cache_worst)
+		cache = dict(pool.imap_unordered(__compute_worst, tasks))
+		util.write(PATH[f"worst_{worst}"], cache)
 
 
 def __compute_state(guess, answer):
