a04f02fa7089014ffb029c16326529f268a2cf4c
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-03-18 15:56:50 +0800

    Update game2048_cache.py

--- cache/game2048_cache.py
+++ cache/game2048_cache.py
@@ -9,6 +9,23 @@ def compute_reversed():
 	__compute_moves(PATH["reversed"], slice(None, None, -1))
 
 
+def __compute_moves(path, piece):
+	cache = {}
+	for tiles in product(range(12), repeat=4):
+		t, merged = [], False
+		for tile in tiles:
+			if tile == 0:
+				continue
+			if t and t[-1] == tile and not merged:
+				t[-1] += 1
+				merged = True
+			else:
+				t.append(tile)
+				merged = False
+		cache[tiles[piece].tobytes()] = np.array(t + [0] * (4 - len(t)), dtype=np.int8)[piece]
+	util.write(path, cache)
+
+
 def compute_mono():
 	cache = {}
 	for tiles in product(range(12), repeat=4):
@@ -31,23 +48,6 @@ def compute_merge():
 		t = t[t != 0]
 		cache[bytes(tiles)] = np.sum(t[:-1][np.diff(t) == 0])
 	util.write(PATH["merge"], cache)
-
-
-def __compute_moves(path, piece):
-	cache = {}
-	for tiles in product(range(12), repeat=4):
-		t, merged = [], False
-		for tile in tiles:
-			if tile == 0:
-				continue
-			if t and t[-1] == tile and not merged:
-				t[-1] += 1
-				merged = True
-			else:
-				t.append(tile)
-				merged = False
-		cache[tiles[piece].tobytes()] = np.array(t + [0] * (4 - len(t)), dtype=np.int8)[piece]
-	util.write(path, cache)
 
 
 if __name__ == "__main__":
