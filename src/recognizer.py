f1bc04e04f94848b1f33dce34b3cb00d6141c15f
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-03-25 23:38:40 +0800

    Update recognizer.py

--- src/recognizer.py
+++ src/recognizer.py
@@ -1,49 +1,45 @@
 from recognizer_ui import Ui_MainWindow
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QImage, QPainter, QPen, QPixmap
+from PyQt6.QtGui import QImage, QPainter, QPen
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from itertools import product
 import cv2
 import numpy as np
 import sys
 import torch
 import util
 
+MODEL = util.join_path(util.RESOURCE, "recognizer", "recognizer.pt")
+
 
 class MyCore(QMainWindow, Ui_MainWindow):
-	SIZE = {"canvas": (400, 400), "thumbnail": (28, 28)}
-	MODEL = util.join_path(util.RESOURCE, "recognizer", "recognizer.pt")
-
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
 		self.setWindowIcon(util.icon("../recognizer/logo"))
 
-		self.label_canvas.setPixmap(util.pixmap(self.SIZE["canvas"], Qt.GlobalColor.white))
-		self.label_thumbnail.setPixmap(util.pixmap(self.SIZE["thumbnail"], Qt.GlobalColor.black))
+		util.pixmap(self.label_canvas, size=self.label_canvas.minimumSize(), color=Qt.GlobalColor.white)
+		util.pixmap(self.label_thumbnail, size=self.label_thumbnail.minimumSize(), color=Qt.GlobalColor.black)
 		self.label_canvas.mousePressEvent = self.mouse_press
 		self.label_canvas.mouseMoveEvent = self.mouse_move
 
 		self.model = NN(NN.MNIST).to(NN.DEVICE)
-		self.model.load_state_dict(torch.load(self.MODEL, map_location=NN.DEVICE))
+		self.model.load_state_dict(torch.load(MODEL, map_location=NN.DEVICE))
 
-	def process(self):
+	def recognize(self):
+		size_canvas = self.label_canvas.minimumWidth(), self.label_canvas.minimumHeight()
+		size_thumbnail = self.label_thumbnail.minimumWidth(), self.label_thumbnail.minimumHeight()
+
 		image = self.label_canvas.pixmap().toImage()
-		bits = image.bits()
+		bits = image.constBits()
 		bits.setsize(image.sizeInBytes())
-		threshold = 255 - np.frombuffer(bits, dtype=np.uint8).reshape((*self.SIZE["canvas"], 4))[:, :, 0]
+		threshold = 255 - np.frombuffer(bits, dtype=np.uint8).reshape((*size_canvas, 4))[:, :, 0]
+		thumbnail = cv2.resize(threshold, size_thumbnail, interpolation=cv2.INTER_AREA)
 
-		pixmap_canvas = self.label_canvas.pixmap()
-		pixmap_canvas.fill(Qt.GlobalColor.white)
-		self.label_canvas.setPixmap(pixmap_canvas)
+		util.pixmap(self.label_canvas, color=Qt.GlobalColor.white)
+		util.pixmap(self.label_thumbnail, image=QImage(thumbnail, *thumbnail.shape, QImage.Format.Format_Indexed8))
 
-		thumbnail = cv2.resize(threshold, self.SIZE["thumbnail"])
-		pixmap_thumbnail = QPixmap().fromImage(QImage(thumbnail, *thumbnail.shape, QImage.Format.Format_Indexed8))
-		self.label_thumbnail.setPixmap(pixmap_thumbnail)
-		return np.array(thumbnail, np.float32)
-
-	def recognize(self, inputs):
 		with torch.no_grad():
+			inputs = np.array(thumbnail, np.float32)
 			tensor = torch.from_numpy(inputs).reshape(-1, NN.MNIST["input"]).to(NN.DEVICE)
 			outputs = self.model(tensor)
 			_, result = torch.max(outputs, dim=1)
@@ -51,7 +47,7 @@ class MyCore(QMainWindow, Ui_MainWindow):
 
 	def keyPressEvent(self, event):
 		if event.key() == Qt.Key.Key_Return:
-			self.recognize(self.process())
+			self.recognize()
 
 	def mouse_press(self, event):
 		self.label_canvas.pos = event.pos()
@@ -59,7 +55,7 @@ class MyCore(QMainWindow, Ui_MainWindow):
 	def mouse_move(self, event):
 		pixmap = self.label_canvas.pixmap()
 		with QPainter(pixmap) as painter:
-			painter.setPen(QPen(Qt.GlobalColor.black, 15))
+			painter.setPen(QPen(Qt.GlobalColor.black, 24))
 			painter.drawLine(self.label_canvas.pos, event.pos())
 		self.label_canvas.setPixmap(pixmap)
 		self.label_canvas.pos = event.pos()
