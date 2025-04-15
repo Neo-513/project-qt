2f649f8545c73236d052ec9345da41b4a011ce2e
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-15 09:35:08 +0800

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,16 +1,14 @@
-from PyQt6.QtCore import QDir, Qt, QUrl
-from PyQt6.QtGui import QAction, QDesktopServices, QIcon
-from PyQt6.QtWidgets import QFileDialog, QLineEdit, QListWidget, QMenu, QMessageBox, QPushButton
+from PyQt6.QtCore import pyqtSignal, QDir, QSize, QTimer, Qt, QUrl
+from PyQt6.QtGui import QAction, QDesktopServices, QImage, QIcon, QPixmap
+from PyQt6.QtWidgets import QApplication, QFileDialog, QLineEdit, QListWidget, QMenu, QMessageBox
 from PyQt6.QtWidgets import QTreeWidget, QTreeWidgetItem
 import json
 import os
 import pickle
 import re
-import shutil
 import sys
 
-DESKTOP = os.path.join(os.path.expanduser("~"), "Desktop").replace("\\", "/")
-RESOURCE = os.path.join(getattr(sys, "_MEIPASS", os.path.abspath("..")), "assets").replace("\\", "/")
+RESOURCE = os.path.join(getattr(sys, "_MEIPASS", os.path.abspath("..")), "static").replace("\\", "/")
 BANNED_CHAR = re.compile(r'[\\/*?:"<>|]')
 
 
@@ -22,11 +20,40 @@ def join_path(*paths):
 	return os.path.join("", *paths).replace("\\", "/")
 
 
-def icon(path: str) -> QIcon:
-	return QIcon(join_path(RESOURCE, "global", f"{path}.png"))
+def icon(path):
+	return QIcon(join_path(RESOURCE, "common", f"{path}.png"))
 
 
-def dialog(msg: str, msg_type: str) -> bool | None:
+def timer(interval, func):
+	t = QTimer()
+	t.setInterval(interval)
+	cast(t).timeout.connect(func)
+	return t
+
+
+def pixmap(label=None, size=None, color=None, image=None):
+	if image:
+		if isinstance(image, str):
+			image = QImage(f"{image}.png")
+		pm = QPixmap().fromImage(image)
+	else:
+		if isinstance(size, QSize):
+			size = size.width(), size.height()
+		if isinstance(size, int):
+			size = (size,) * 2
+		pm = QPixmap(*tuple(size)) if size else label.pixmap()
+		pm.fill(color) if color else None
+	label.setPixmap(pm) if label else None
+	return pm
+
+
+def signal(func):
+	s = pyqtSignal()
+	cast(s).connect(func)
+	return s
+
+
+def dialog(msg, msg_type):
 	message_box = QMessageBox()
 	message_box.setText(msg)
 	message_box.setWindowIcon(icon(msg_type))
@@ -37,22 +64,15 @@ def dialog(msg: str, msg_type: str) -> bool | None:
 	return message_box.exec() == QMessageBox.StandardButton.Yes
 
 
-def button(push_button: QPushButton, func: callable, ico: str = None, tip: str = None):
+def button(push_button, func, ico=None, tip=None, ico_size=None):
 	cast(push_button).clicked.connect(func)
 	push_button.setIcon(icon(ico)) if ico else None
 	push_button.setToolTip(tip) if tip else None
+	push_button.setIconSize(QSize(ico_size, ico_size)) if ico_size is not None else None
 	push_button.setCursor(Qt.CursorShape.PointingHandCursor)
 
 
-def add_action(line_edit: QLineEdit, ico: str, text: str, func: callable, position: QLineEdit.ActionPosition = QLineEdit.ActionPosition.TrailingPosition):
-	action = QAction(line_edit)
-	action.setIcon(icon(ico))
-	action.setToolTip(text)
-	cast(action).triggered.connect(func)
-	line_edit.addAction(action, position)
-
-
-def select_folder(line_edit: QLineEdit, push_button: QPushButton, func: callable, clean=False):
+def select_folder(line_edit, push_button, func):
 	def _select():
 		folder_path = QFileDialog.getExistingDirectory(directory=line_edit.text())
 		if not os.path.exists(folder_path):
@@ -60,40 +80,18 @@ def select_folder(line_edit: QLineEdit, push_button: QPushButton, func: callable
 		line_edit.setText(folder_path)
 		func() if func else None
 
-	def _clean():
-		folder_path = line_edit.text()
-		if not os.path.exists(folder_path):
-			return
-		if dialog(f"确认清理路径[{folder_path}]下所有文件?", "warning"):
-			shutil.rmtree(folder_path)
-			os.mkdir(folder_path)
-			dialog("清理完成!", "success")
-		func() if func else None
+	action = QAction(line_edit)
+	action.setIcon(icon("open_folder"))
+	action.setToolTip("打开文件夹")
+	cast(action).triggered.connect(lambda: open_folder(line_edit.text()))
 
-	add_action(line_edit, "open_folder", "打开文件夹", lambda: open_folder(line_edit.text()))
-	add_action(line_edit, "clean", "清理文件", _clean) if clean else None
 	button(push_button, _select, "folder") if push_button else None
 	line_edit.setReadOnly(True)
 	line_edit.setToolTip(line_edit.placeholderText())
+	line_edit.addAction(action, QLineEdit.ActionPosition.TrailingPosition)
 
 
-def pwd(line_edit: QLineEdit):
-	def _func():
-		if line_edit.echoMode() == QLineEdit.EchoMode.Normal:
-			line_edit.setEchoMode(QLineEdit.EchoMode.Password)
-			line_edit.setReadOnly(True)
-			line_edit.actions()[0].setIcon(icon("hide"))
-		else:
-			line_edit.setEchoMode(QLineEdit.EchoMode.Normal)
-			line_edit.setReadOnly(False)
-			line_edit.actions()[0].setIcon(icon("show"))
-
-	add_action(line_edit, "hide", "密码", _func, position=QLineEdit.ActionPosition.LeadingPosition)
-	line_edit.setEchoMode(QLineEdit.EchoMode.Password)
-	line_edit.setReadOnly(True)
-
-
-def open_folder(folder_path: str):
+def open_folder(folder_path):
 	if os.path.isfile(folder_path):
 		folder_path = os.path.dirname(folder_path)
 	if not os.path.exists(folder_path):
@@ -101,37 +99,48 @@ def open_folder(folder_path: str):
 	QDesktopServices.openUrl(QUrl.fromLocalFile(folder_path))
 
 
-class FileIO:
-	@staticmethod
-	def read(file_path, encoding = "utf-8"):
-		file_type = os.path.splitext(file_path)[1]
-		if file_type == ".pkl":
-			with open(file_path, mode="rb") as file:
-				return pickle.load(file)
-		elif file_type == ".json":
-			with open(file_path, mode="r", encoding=encoding) as file:
-				return json.load(file)
-		else:
-			with open(file_path, mode="r", encoding=encoding) as file:
-				return file.read()
+def screen_info():
+	app = QApplication([])
+	screen = app.primaryScreen()
+	app.quit()
+	return {
+		"size": (screen.size().width(), screen.size().height()),
+		"dpr": screen.devicePixelRatio()
+	}
 
-	@staticmethod
-	def write(file_path, datas, encoding = "utf-8"):
-		file_type = os.path.splitext(file_path)[1]
-		if file_type == ".pkl":
-			with open(file_path, mode="wb") as file:
-				pickle.dump(datas, cast(file))
-		elif file_type == ".json":
-			with open(file_path, mode="w", encoding=encoding) as file:
-				json.dump(datas, cast(file))
-		else:
-			with open(file_path, mode="w", encoding=encoding) as file:
-				file.write(datas)
+
+def read(file_path):
+	extension = os.path.splitext(file_path)[1]
+	if extension == ".pkl":
+		with open(file_path, mode="rb") as file:
+			datas = pickle.load(file)
+	elif extension == ".json":
+		with open(file_path, mode="r", encoding="utf-8") as file:
+			datas = json.load(file)
+	else:
+		with open(file_path, mode="r", encoding="utf-8") as file:
+			datas = file.read()
+	print(f"[READ] {file_path}")
+	return datas
+
+
+def write(file_path, datas):
+	extension = os.path.splitext(file_path)[1]
+	if extension == ".pkl":
+		with open(file_path, mode="wb") as file:
+			pickle.dump(datas, cast(file))
+	elif extension == ".json":
+		with open(file_path, mode="w", encoding="utf-8") as file:
+			json.dump(datas, cast(file))
+	else:
+		with open(file_path, mode="w", encoding="utf-8") as file:
+			file.write(datas)
+	print(f"[WRITE] {file_path}")
 
 
 class Tree:
 	@staticmethod
-	def scan(tree_widget: QTreeWidget, folder_path: str, check=False):
+	def scan(tree_widget, folder_path, check=False):
 		def _func(path_parent, parent):
 			entry = QDir(path_parent).entryList(QDir.Filter.Dirs | QDir.Filter.Files | QDir.Filter.NoDotAndDotDot)
 			entry.sort(key=lambda x: tuple(reversed(os.path.splitext(x))))
@@ -160,7 +169,7 @@ class Tree:
 			_func(folder_path, cast(None))
 
 	@staticmethod
-	def select(tree_widget: QTreeWidget):
+	def select(tree_widget):
 		def _func(item):
 			cast(tree_widget).itemChanged.disconnect(_func)
 
@@ -189,7 +198,7 @@ class Tree:
 
 class Menu:
 	@staticmethod
-	def menu(widget, select: bool = False, folder: bool = False, func: callable = None):
+	def menu(widget, select=False, folder=False, func=None):
 		def _func(point):
 			widget.setProperty("menu", QMenu(widget))
 			item = widget.itemAt(point)
@@ -213,7 +222,7 @@ class Menu:
 		cast(widget).customContextMenuRequested.connect(_func)
 
 	@staticmethod
-	def select_all(widget: QTreeWidget | QListWidget):
+	def select_all(widget):
 		if isinstance(widget, QTreeWidget):
 			for i in range(widget.topLevelItemCount()):
 				widget.topLevelItem(i).setCheckState(0, Qt.CheckState.Checked)
@@ -222,7 +231,7 @@ class Menu:
 				widget.item(i).setCheckState(Qt.CheckState.Checked)
 
 	@staticmethod
-	def unselect_all(widget: QTreeWidget | QListWidget):
+	def unselect_all(widget):
 		if isinstance(widget, QTreeWidget):
 			for i in range(widget.topLevelItemCount()):
 				widget.topLevelItem(i).setCheckState(0, Qt.CheckState.Unchecked)
@@ -231,45 +240,15 @@ class Menu:
 				widget.item(i).setCheckState(Qt.CheckState.Unchecked)
 
 	@staticmethod
-	def open_folder(widget: QTreeWidget | QListWidget):
+	def open_folder(widget):
 		if isinstance(widget, QTreeWidget):
 			open_folder(widget.currentItem().toolTip(0))
 		elif isinstance(widget, QListWidget):
 			open_folder(widget.currentItem().toolTip())
 
 	@staticmethod
-	def add(widget, ico: str, text: str, func: callable):
+	def add(widget, ico, text, func):
 		action = QAction(text, widget)
 		action.setIcon(icon(ico))
 		cast(action).triggered.connect(func)
 		widget.addAction(action)
-
-
-class Sync:
-	@staticmethod
-	def scroll(self, widget_old, widget_new):
-		def _func(value):
-			widget = self.sender()
-			cast(widgets[widget]).valueChanged.disconnect(_func)
-			widgets[widget].setValue(value)
-			cast(widgets[widget]).valueChanged.connect(_func)
-
-		vbar_old, hbar_old = widget_old.verticalScrollBar(), widget_old.horizontalScrollBar()
-		vbar_new, hbar_new = widget_new.verticalScrollBar(), widget_new.horizontalScrollBar()
-		widgets = {vbar_old: vbar_new, hbar_old: hbar_new, vbar_new: vbar_old, hbar_new: hbar_old}
-
-		cast(vbar_old).valueChanged.connect(_func)
-		cast(vbar_new).valueChanged.connect(_func)
-		cast(hbar_old).valueChanged.connect(_func)
-		cast(hbar_new).valueChanged.connect(_func)
-
-
-	@staticmethod
-	def select(self, widget_old: QListWidget, widget_new: QListWidget):
-		def _func():
-			widget = self.sender()
-			widgets[widget].setCurrentRow(widget.currentRow())
-
-		widgets = {widget_old: widget_new, widget_new: widget_old}
-		cast(widget_old).itemSelectionChanged.connect(_func)
-		cast(widget_new).itemSelectionChanged.connect(_func)
