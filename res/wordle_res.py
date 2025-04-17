83f152118e2be21c6d52e34ab340379fbefc0a7b
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-17 23:54:04 +0800

    Update wordle_res.py

--- res/wordle_res.py
+++ res/wordle_res.py
@@ -6,29 +6,31 @@ from string import ascii_lowercase
 
 def resource_tile():
 	application = QApplication([])
+	dpr = application.primaryScreen().devicePixelRatio()
 	for text in ascii_lowercase:
-		__tile(QColor(120, 124, 126), QColor(120, 124, 126), Qt.GlobalColor.white, text, f"0_{text}")
-		__tile(QColor(198, 180, 86), QColor(198, 180, 86), Qt.GlobalColor.white, text, f"1_{text}")
-		__tile(QColor(112, 169, 97), QColor(112, 169, 97), Qt.GlobalColor.white, text, f"2_{text}")
-		__tile(QColor(211, 214, 218), Qt.GlobalColor.white, QColor(181, 184, 188), text, f"hint_{text}")
-		__tile(QColor(200, 0, 0), Qt.GlobalColor.white, QColor(200, 0, 0), text, f"hit_{text}")
-		__tile(Qt.GlobalColor.black, Qt.GlobalColor.white, Qt.GlobalColor.black, text, f"text_{text}")
-	__tile(QColor(211, 214, 218), Qt.GlobalColor.white, Qt.GlobalColor.black, "", f"nontext")
+		__tile(QColor(120, 124, 126), QColor(120, 124, 126), Qt.GlobalColor.white, text, f"0_{text}", dpr)
+		__tile(QColor(198, 180, 86), QColor(198, 180, 86), Qt.GlobalColor.white, text, f"1_{text}", dpr)
+		__tile(QColor(112, 169, 97), QColor(112, 169, 97), Qt.GlobalColor.white, text, f"2_{text}", dpr)
+		__tile(QColor(211, 214, 218), Qt.GlobalColor.white, QColor(181, 184, 188), text, f"hint_{text}", dpr)
+		__tile(QColor(200, 0, 0), Qt.GlobalColor.white, QColor(200, 0, 0), text, f"hit_{text}", dpr)
+		__tile(Qt.GlobalColor.black, Qt.GlobalColor.white, Qt.GlobalColor.black, text, f"text_{text}", dpr)
+	__tile(QColor(211, 214, 218), Qt.GlobalColor.white, Qt.GlobalColor.black, "", f"nontext", dpr)
 	application.quit()
 
 
-def __tile(border_color, background_color, color, text, name):
-	pixmap = QPixmap(52, 52)
+def __tile(border_color, background_color, color, text, name, dpr):
+	sizes = {"block": round(dpr * 52), "gap": round(dpr * 2), "groove": round(dpr * 48), "font": round(dpr * 21)}
+	pixmap = QPixmap(sizes["block"], sizes["block"])
 	pixmap.fill(background_color)
 	with QPainter(pixmap) as painter:
-		painter.setFont(QFont("nyt-franklin", 21, QFont.Weight.Bold))
+		painter.setFont(QFont("nyt-franklin", sizes["font"], QFont.Weight.Bold))
 		painter.setPen(Qt.PenStyle.NoPen)
 		painter.setBrush(border_color)
-		painter.drawRect(0, 0, 52, 52)
+		painter.drawRect(0, 0, sizes["block"], sizes["block"])
 		painter.setBrush(background_color)
-		painter.drawRect(2, 2, 48, 48)
+		painter.drawRect(sizes["gap"], sizes["gap"], sizes["groove"], sizes["groove"])
 		painter.setPen(color)
-		painter.drawText(0, 0, 52, 52, Qt.AlignmentFlag.AlignCenter, text.upper())
+		painter.drawText(0, 0, sizes["block"], sizes["block"], Qt.AlignmentFlag.AlignCenter, text.upper())
 	pixmap.save(f"{name}.png")
 
 
