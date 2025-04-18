751863d7791919783f191f120ae41f783104cecc
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-18 16:48:40 +0800

    Update sudoku_cache.py

--- cache/sudoku_cache.py
+++ cache/sudoku_cache.py
@@ -9,22 +9,6 @@ def compute_graph():
 	cache.tofile(PATH["graph"])
 
 
-def compute_background():
-	application = QApplication([])
-	pixmap = util.pixmap(size=540, color=QColor(243, 243, 243))
-	with QPainter(pixmap) as painter:
-		painter.setPen(QPen(Qt.GlobalColor.gray, 1))
-		for pos in product(range(9), repeat=2):
-			painter.drawRect(QRect(*np.array((*pos[::-1], 1, 1)) * MyCore.SIZE["block"]))
-		painter.setPen(QPen(Qt.GlobalColor.black, 2))
-		for pos in product(range(3), repeat=2):
-			painter.drawRect(QRect(*np.array((*pos[::-1], 1, 1)) * MyCore.SIZE["block"] * 3))
-		painter.drawRect(1, 1, pixmap.width() - 2, pixmap.height() - 2)
-	pixmap.save("background.png")
-	application.quit()
-
-
 if __name__ == "__main__":
 	# compute_graph()
-	# compute_background()
 	pass
