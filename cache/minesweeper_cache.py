8794082ef6a3aa5e33ee7c89b52956125f55cb43
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-19 13:45:08 +0800

    Create minesweeper_cache.py

---
+++ cache/minesweeper_cache.py
@@ -0,0 +1,19 @@
+from itertools import product
+import pickle
+
+
+def compute_surrounding():
+	cache = {"easy": {}, "medium": {}, "hard": {}}
+	difficulty = {"easy": (9, 9), "medium": (16, 16), "hard": (16, 30)}
+	offset = tuple(pos for pos in product(range(-1, 2), repeat=2) if pos != (0, 0))
+	for d, (r, c) in difficulty.items():
+		for x, y in product(range(r), range(c)):
+			surrounding = tuple((x + i, y + j) for i, j in offset if 0 <= (x + i) < r and 0 <= (y + j) < c)
+			if surrounding:
+				cache[d][x, y] = surrounding
+	with open("surrounding.pkl", mode="wb") as file:
+		pickle.dump(cache, file)
+
+
+if __name__ == "__main__":
+	compute_surrounding()
