beaad9b1cb6281f2d9e44d0b67cb91a8f509faaf
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-20 13:31:48 +0800

    Update minesweeper_cache.py

--- cache/minesweeper_cache.py
+++ cache/minesweeper_cache.py
@@ -1,17 +1,29 @@
 from itertools import product
 import pickle
 
-
 def compute_surrounding():
 	cache = {"easy": {}, "medium": {}, "hard": {}}
-	difficulty = {"easy": (9, 9), "medium": (16, 16), "hard": (16, 30)}
 	offset = tuple(pos for pos in product(range(-1, 2), repeat=2) if pos != (0, 0))
-	for d, (r, c) in difficulty.items():
+	for d, (r, c) in {"easy": (9, 9), "medium": (16, 16), "hard": (16, 30)}.items():
 		for x, y in product(range(r), range(c)):
 			cache[d][x, y] = tuple((x + i, y + j) for i, j in offset if 0 <= (x + i) < r and 0 <= (y + j) < c)
 	with open("surrounding.pkl", mode="wb") as file:
 		pickle.dump(cache, file)
 
 
+def compute_platform():
+	cache = {"easy": {}, "medium": {}, "hard": {}}
+	with open("surrounding.pkl", mode="rb") as file:
+		cache_surrounding = pickle.load(file)
+	for d, (r, c) in {"easy": (9, 9), "medium": (16, 16), "hard": (16, 30)}.items():
+		s = set(cache_surrounding[d])
+		for x, y in product(range(r), range(c)):
+			cache[d][x, y] = tuple(s - {(x + i, y + j) for i, j in product(range(-2, 3), repeat=2)})
+	with open("platform.pkl", mode="wb") as file:
+		pickle.dump(cache, file)
+
+
 if __name__ == "__main__":
-	compute_surrounding()
+	# compute_surrounding()
+	# compute_platform()
+	pass
