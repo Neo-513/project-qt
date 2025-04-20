a03b51be263525f2207f0f24797d444aaf270a08
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-20 15:32:08 +0800

    Update minesweeper.py

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -1,247 +1,198 @@
 from minesweeper_ui import Ui_MainWindow
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QSizePolicy
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 from itertools import product
+import numpy as np
+import os
 import random
 import sys
 import util
 
-_QSS = (
-	"QPushButton {font-family: 'Rockwell Extra Bold'; font-size: 20px; font-weight: bold; icon-size: 20px}"
-	"QPushButton {border-radius: 5px; border: 1px solid %s; background-color: %s}"
-)
-QSS_UNEXPLORED = _QSS % ("black", "rgb(100,150,200)") + "QPushButton:hover {background-color: rgb(70,120,170)}"
-QSS_EXPLORED = _QSS % ("rgb(127,127,127)", "rgb(210,210,210)") + "QPushButton {color: %s}"
-QSS_HINT = _QSS % ("black", "rgb(40,90,140)")
-
-DIFFICULTY = {"easy": (9, 9, 10), "medium": (16, 16, 40), "hard": (16, 30, 99)}
-COLOR = {
-	1: "rgb(045,015,255)", 2: "rgb(034,127,000)", 3: "rgb(243,018,022)", 4: "rgb(018,003,129)",
-	5: "rgb(122,004,006)", 6: "rgb(041,128,129)", 7: "rgb(000,000,000)", 8: "rgb(124,124,124)"
+PATH = {
+	"surrounding": util.join_path(util.RESOURCE, "minesweeper", "surrounding.pkl"),
+	"platform": util.join_path(util.RESOURCE, "minesweeper", "platform.pkl")
 }
-
-SCREEN_SIZE = util.screen()["size"]
-BLOCK_SIZE = 35
-
-PATH_SURROUNDING = util.join_path(util.RESOURCE, "minesweeper", "surrounding.pkl")
-CACHE_SURROUNDING = util.read(PATH_SURROUNDING)
+CACHE = {
+	"surrounding": util.read(PATH["surrounding"]) if os.path.exists(PATH["surrounding"]) else None,
+	"platform": util.read(PATH["platform"]) if os.path.exists(PATH["platform"]) else None
+}
 
 
 class MyCore(QMainWindow, Ui_MainWindow):
-	row_count, col_count, mine_count = None, None, None
-	minefield, amount_pressed, amount_flagged = None, None, None
-	difficulty = None
+	SIZE = {"block": 35, "gap": 1}
+	MESSAGE = "剩余: %s  计时: %s"
+	DIFFICULTY = {"easy": (9, 9, 10), "medium": (16, 16, 40), "hard": (16, 30, 99)}
+	COLOR = (
+		"black",
+		"rgb(045,015,255)", "rgb(034,127,000)", "rgb(243,018,022)", "rgb(018,003,129)",
+		"rgb(122,004,006)", "rgb(041,128,129)", "rgb(000,000,000)", "rgb(124,124,124)"
+	)
+
+	_QSS = "QPushButton {icon-size: 20px; border-radius: 5px; border: 1px solid %s; background-color: %s}"
+	QSS = {
+		"unexplored": _QSS % ("black", "rgb(100, 150, 200)") + "QPushButton:hover {background-color: rgb(70, 120, 170)}",
+		"explored": _QSS % ("rgb(127, 127, 127)", "rgb(210, 210, 210)") + "QPushButton {color: %s}",
+		"hint": _QSS % ("black", "rgb(40, 90, 140)")
+	}
 
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
 		self.setWindowIcon(util.icon("../minesweeper/logo"))
 
-		self.radioButton_easy.clicked.connect(self.switch)
-		self.radioButton_medium.clicked.connect(self.switch)
-		self.radioButton_hard.clicked.connect(self.switch)
+		self.radioButton_easy.clicked.connect(lambda: self.switch("easy"))
+		self.radioButton_medium.clicked.connect(lambda: self.switch("medium"))
+		self.radioButton_hard.clicked.connect(lambda: self.switch("hard"))
 
-		self.timer = util.timer(10, lambda: MyStatic.timeout(self))
+		r, c, _ = self.DIFFICULTY["hard"]
+		self.buttons = {pos: util.cast(QPushButton()) for pos in product(range(r), range(c))}
+		for pos, button in self.buttons.items():
+			button.pos, button.pressed, button.flagged, button.hint = pos, False, False, tuple()
+			button.mousePressEvent = lambda event, b=button: self.press_mouse(event, b)
+			button.mouseReleaseEvent = lambda event, b=button: self.release_mouse(event, b)
+			button.setFixedSize(self.SIZE["block"], self.SIZE["block"])
+			button.setFont(QFont("Rockwell Extra Bold", 15, QFont.Weight.Bold))
+			self.gridLayout.addWidget(button, *pos)
+			if pos[0] >= self.DIFFICULTY["easy"][0] or pos[1] >= self.DIFFICULTY["easy"][1]:
+				button.hide()
 
-		rc, cc, _ = DIFFICULTY["hard"]
-		for i, j in product(range(rc), range(cc)):
-			widget = MineBlock(i, j)
-			widget.func_sweep = lambda x=i, y=j: self.func_sweep(x, y)
-			widget.func_flag = lambda w=widget: self.func_flag(w)
-			widget.func_hint = lambda x=i, y=j: self.func_hint(x, y)
-			self.gridLayout.addWidget(widget, i, j)
+		self.MINEFIELD = {d: np.zeros((r, c), dtype=np.uint8) for d, (r, c, _) in self.DIFFICULTY.items()}
+		self.WINDOW_SIZE = {d: (
+			self.SIZE["block"] * c + self.SIZE["gap"] * (c - 1) + 18,
+			self.SIZE["block"] * r + self.SIZE["gap"] * (r - 1) + 55
+		) for d, (r, c, _) in self.DIFFICULTY.items()}
+		self.WINDOW_POS = {d: (
+			(app.primaryScreen().size().width() - self.WINDOW_SIZE[d][0]) // 2,
+			(app.primaryScreen().size().height() - self.WINDOW_SIZE[d][1]) // 2
+		) for d in self.DIFFICULTY}
+
+		self.difficulty = "easy"
+		self.r = self.c = self.m = self.minefield = self.surrounding = self.platform = None
+		self.pressed = self.flagged = 0
+		self.timer = util.timer(10, self.timeout)
 		self.radioButton_easy.click()
 
-	def switch(self):
-		self.difficulty = self.sender().objectName().split("_")[-1]
-		self.row_count, self.col_count, self.mine_count = DIFFICULTY[self.difficulty]
-		row_max, col_max, _ = DIFFICULTY["hard"]
-		row_min, col_min, _ = DIFFICULTY["easy"]
+	def switch(self, difficulty):
+		r_pre, c_pre, _ = self.DIFFICULTY[self.difficulty]
+		r_sub, c_sub, _ = self.DIFFICULTY[difficulty]
+		if c_pre != c_sub:
+			r_max, c_max = max(r_pre, r_sub), max(c_pre, c_sub)
+			r_min, c_min = min(r_pre, r_sub), min(c_pre, c_sub)
+			hidden = c_pre > c_sub
+			for pos in product(range(r_max), range(c_max)):
+				if pos[0] >= r_min or pos[1] >= c_min:
+					self.buttons[pos].setHidden(hidden)
 
-		for i, j in product(range(row_max), range(col_max)):
-			if i < row_min and j < col_min:
-				continue
-			self.gridLayout.itemAtPosition(i, j).widget().hide()
+		self.difficulty = difficulty
+		self.r, self.c, self.m = self.DIFFICULTY[self.difficulty]
+		self.minefield = self.MINEFIELD[self.difficulty]
+		self.surrounding = CACHE["surrounding"][self.difficulty]
+		self.platform = CACHE["platform"][self.difficulty]
 
-		for i, j in product(range(row_max), range(col_max)):
-			if i < row_min and j < col_min:
-				continue
-			if i >= self.row_count or j >= self.col_count:
-				continue
-			self.gridLayout.itemAtPosition(i, j).widget().show()
-
-		window_size = self.col_count * (BLOCK_SIZE + 1) + 17, self.row_count * (BLOCK_SIZE + 1) + 54
-		self.window().setFixedSize(*window_size)
-		window_pos = (SCREEN_SIZE[0] - self.window().width()) // 2, (SCREEN_SIZE[1] - self.window().height()) // 2
-		self.window().move(*window_pos)
+		self.window().setFixedSize(*self.WINDOW_SIZE[self.difficulty])
+		self.window().move(*self.WINDOW_POS[self.difficulty])
 		self.restart()
 
 	def restart(self):
-		self.amount_pressed = 0
-		self.amount_flagged = 0
+		self.minefield.fill(0)
+		self.pressed = self.flagged = 0
 
 		self.timer.stop()
 		self.timer.time = 0
 
-		for pos in product(range(self.row_count), range(self.col_count)):
-			widget = self.gridLayout.itemAtPosition(*pos).widget()
-			widget.setText("")
-			widget.status_pressed = False
-			widget.status_flagged = False
-			widget.setIcon(QIcon())
-			widget.setStyleSheet(QSS_UNEXPLORED)
-		MyStatic.message(self)
+		for pos, button in self.buttons.items():
+			button.setText("")
+			button.pressed = button.flagged = False
+			button.setIcon(util.icon(None))
+			button.setStyleSheet(self.QSS["unexplored"])
+		self.statusbar.showMessage(self.MESSAGE % (self.m, 0))
 
-	def func_sweep(self, x, y):
-		if self.minefield[x][y] == 9:
-			return self.judge(False)
-		self.expand(x, y)
-		MyStatic.message(self)
-
-		if self.amount_pressed == self.row_count * self.col_count - self.mine_count:
-			return self.judge(True)
-
-	def func_flag(self, w):
-		w.status_flagged = not w.status_flagged
-		w.setIcon(util.icon("../minesweeper/flag") if w.status_flagged else QIcon())
-		self.amount_flagged += 1 if w.status_flagged else -1
-		MyStatic.message(self)
-
-	def func_hint(self, x, y):
-		if not self.minefield[x][y]:
+	def press_mouse(self, event, button):
+		if event.buttons() not in (Qt.MouseButton.LeftButton, Qt.MouseButton.RightButton):
+			return
+		if event.buttons() == Qt.MouseButton.LeftButton and button.flagged:
 			return
 
-		flagged_amount = 0
-		unflagged_pos = []
+		if event.buttons() == Qt.MouseButton.LeftButton and not self.pressed:
+			mines = random.sample(self.platform[button.pos], self.m)
+			self.minefield[*zip(*mines)] = 9
+			for pos in mines:
+				for s in self.surrounding[pos]:
+					self.minefield[s] += self.minefield[s] != 9
+			self.timer.start()
 
-		for pos in CACHE_SURROUNDING[self.difficulty][x, y]:
-			widget = self.gridLayout.itemAtPosition(pos[0], pos[1]).widget()
-			if widget.status_pressed:
-				continue
-			if widget.status_flagged:
-				flagged_amount += 1
+		if not button.pressed:
+			if event.buttons() == Qt.MouseButton.LeftButton:
+				self.sweep(button.pos)
 			else:
-				unflagged_pos.append(pos)
+				button.flagged = not button.flagged
+				button.setIcon(util.icon("../minesweeper/flag" if button.flagged else None))
+				self.flagged += 1 if button.flagged else -1
+				self.statusbar.showMessage(self.MESSAGE % (self.m - self.flagged, round(self.timer.time)))
+		elif self.minefield[button.pos] != 0:
+			surrounding = self.surrounding[button.pos]
+			unflagged = tuple(s for s in surrounding if not self.buttons[s].pressed and not self.buttons[s].flagged)
+			if self.minefield[button.pos] == sum(self.buttons[s].flagged for s in surrounding):
+				for u in unflagged:
+					self.sweep(u)
+			else:
+				for u in unflagged:
+					self.buttons[u].setStyleSheet(self.QSS["hint"])
+				self.buttons[button.pos].hint = unflagged
 
-		if flagged_amount == self.minefield[x][y]:
-			for pos in unflagged_pos:
-				if self.amount_pressed:
-					self.func_sweep(pos[0], pos[1])
-		else:
-			for pos in unflagged_pos:
-				self.gridLayout.itemAtPosition(pos[0], pos[1]).widget().setStyleSheet(QSS_HINT)
-			self.gridLayout.itemAtPosition(x, y).widget().hint_pos = unflagged_pos
+	def release_mouse(self, _, button):
+		for pos in button.hint:
+			self.buttons[pos].setStyleSheet(self.QSS["unexplored"])
 
-	def expand(self, x, y):
-		if self.minefield[x][y] == 9:
-			return self.judge(False)
-		if (x, y) not in CACHE_SURROUNDING[self.difficulty]:
-			return
+	def sweep(self, pos):
+		if self.minefield[pos] == 9:
+			return self.judge("You lose", "error")
 
-		widget = self.gridLayout.itemAtPosition(x, y).widget()
-		if widget.status_pressed:
-			return
-		if widget.status_flagged:
-			return
+		self.expand(pos)
+		self.statusbar.showMessage(self.MESSAGE % (self.m - self.flagged, round(self.timer.time)))
 
-		mine_value = self.minefield[x][y]
-		widget.status_pressed = True
-		widget.setText(str(mine_value).strip("0"))
-		widget.setStyleSheet(QSS_EXPLORED % COLOR.get(mine_value, "black"))
-		self.amount_pressed += 1
+		if self.pressed == self.r * self.c - self.m:
+			return self.judge("You win", "success")
 
-		if not mine_value:
-			for pos in CACHE_SURROUNDING[self.difficulty][x, y]:
-				self.expand(pos[0], pos[1])
-
-	def judge(self, win):
-		for i, j in product(range(self.row_count), range(self.col_count)):
-			widget = self.gridLayout.itemAtPosition(i, j).widget()
-			if self.minefield[i][j] == 9 and not widget.status_flagged:
-				if win:
-					widget.setIcon(util.icon("../minesweeper/flag"))
-				else:
-					widget.setIcon(util.icon("../minesweeper/mine"))
-			elif self.minefield[i][j] != 9 and widget.status_flagged:
-				widget.setIcon(util.icon("../minesweeper/misjudged"))
-
+	def judge(self, msg, msg_type):
+		for pos in product(range(self.r), range(self.c)):
+			if self.minefield[pos] == 9 and not self.buttons[pos].flagged:
+				self.buttons[pos].setIcon(util.icon("../minesweeper/flag" if msg_type == "success" else "../minesweeper/mine"))
+			elif self.minefield[pos] != 9 and self.buttons[pos].flagged:
+				self.buttons[pos].setIcon(util.icon("../minesweeper/misjudged"))
 		self.timer.stop()
-		self.amount_flagged = self.mine_count
-		MyStatic.message(self)
-		util.dialog("You win", "success") if win else util.dialog("You lose", "error")
+		self.statusbar.showMessage(self.MESSAGE % (0, round(self.timer.time)))
+		util.dialog(msg, msg_type)
 		self.restart()
 
+	def expand(self, pos):
+		button = self.buttons[pos]
+		if button.pressed or button.flagged:
+			return
+
+		button.pressed = True
+		button.setText(str(self.minefield[pos]).strip("0"))
+		button.setStyleSheet(self.QSS["explored"] % self.COLOR[self.minefield[pos]])
+		self.pressed += 1
+
+		if self.minefield[pos] == 0:
+			for s in self.surrounding[pos]:
+				self.expand(s)
+
+	def timeout(self):
+		self.timer.time += 0.01
+		if self.timer.time >= 999:
+			self.timer.stop()
+		self.statusbar.showMessage(self.MESSAGE % (self.m - self.flagged, round(self.timer.time)))
+
 	def leaveEvent(self, event):
-		super().leaveEvent(event)
-		if self.amount_pressed and self.timer.isActive():
+		if self.pressed and self.timer.isActive():
 			self.timer.stop()
 
 	def enterEvent(self, event):
-		super().enterEvent(event)
-		if self.amount_pressed and not self.timer.isActive():
+		if self.pressed and not self.timer.isActive():
 			self.timer.start()
-
-class MyStatic:
-	@staticmethod
-	def reset(x, y):
-		mine_poses = [pos for pos in product(range(my_core.row_count), range(my_core.col_count))]
-		for i, j in product(range(-2, 3), repeat=2):
-			if (x + i, y + j) in CACHE_SURROUNDING[my_core.difficulty]:
-				mine_poses.remove((x + i, y + j))
-		mine_poses = random.sample(mine_poses, my_core.mine_count)
-
-		my_core.minefield = [[((i, j) in mine_poses) * 9 for j in range(my_core.col_count)] for i in range(my_core.row_count)]
-		for mine_pos in mine_poses:
-			for i, j in CACHE_SURROUNDING[my_core.difficulty][mine_pos[0], mine_pos[1]]:
-				if my_core.minefield[i][j] != 9:
-					my_core.minefield[i][j] += 1
-
-	@staticmethod
-	def message(self):
-		rest = self.mine_count - self.amount_flagged
-		cost = round(self.timer.time)
-		self.statusbar.showMessage(f"剩余: {rest:02}  用时: {cost:03}")
-
-	@staticmethod
-	def timeout(self):
-		self.timer.time += self.timer.interval() / 1000
-		if self.timer.time >= 999:
-			self.timer.stop()
-		MyStatic.message(self)
-
-
-class MineBlock(QPushButton):
-	func_sweep, func_flag, func_hint = None, None, None
-	status_pressed, status_flagged = None, None
-	hint_pos = []
-
-	def __init__(self, x, y):
-		super().__init__()
-		self.setMinimumSize(BLOCK_SIZE, BLOCK_SIZE)
-		self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-		self.x, self.y = x, y
-
-	def mousePressEvent(self, e):
-		super().mousePressEvent(e)
-		if e.buttons() == Qt.MouseButton.LeftButton and not self.status_flagged:
-			if not my_core.amount_pressed:
-				my_core.timer.start()
-				MyStatic.reset(self.x, self.y)
-			if not self.status_pressed:
-				self.func_sweep()
-			else:
-				self.func_hint()
-		elif e.buttons() == Qt.MouseButton.RightButton:
-			if not self.status_pressed:
-				self.func_flag()
-			else:
-				self.func_hint()
-
-	def mouseReleaseEvent(self, e):
-		for pos in self.hint_pos:
-			my_core.gridLayout.itemAtPosition(pos[0], pos[1]).widget().setStyleSheet(QSS_UNEXPLORED)
-		self.hint_pos.clear()
 
 
 if __name__ == "__main__":
