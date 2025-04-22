1ebef8d6effcf5ee2a616c64f76007fdd1bea2df
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-23 01:40:32 +0800

    Update sudoku.py

--- src/sudoku.py
+++ src/sudoku.py
@@ -5,20 +5,19 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from collections import defaultdict
 from itertools import product
 import numpy as np
-import os
 import sys
 import util
 
-PATH = {"graph": util.join_path(util.RESOURCE, "sudoku", "cache_graph.bin")}
-CACHE = {"graph": np.fromfile(PATH["graph"], dtype=np.uint8).reshape(729, 324) if os.path.exists(PATH["graph"]) else None}
+CACHE = {"graph": np.fromfile(util.join_path(util.RESOURCE, "sudoku", "cache_graph.bin"), dtype=np.uint8).reshape(729, 324)}
 
 
 class MyCore(QMainWindow, Ui_MainWindow):
 	SIZE = 60
 	KEY = {getattr(Qt.Key, f"Key_{i + 1}"): i + 1 for i in range(9)}
+	POSITION = {(i, j): (j * 60, i * 60) for i, j in product(range(9), repeat=2)}
 	IMG = {
-		"black": tuple(util.image(f"../sudoku/black{i}") for i in range(9)),
-		"gray": tuple(util.image(f"../sudoku/gray{i}") for i in range(9)),
+		"black": [util.image(f"../sudoku/black{i}") for i in range(9)],
+		"gray": [util.image(f"../sudoku/gray{i}") for i in range(9)],
 		"selection": util.image(f"../sudoku/selection")
 	}
 
@@ -31,67 +30,62 @@ class MyCore(QMainWindow, Ui_MainWindow):
 		self.label_background.setPixmap(util.pixmap("../sudoku/background"))
 		self.label_cover = util.mask(self.label_canvas, (9, 9), pointer=True)
 		self.label_cover.mousePressEvent = self.press_mouse
-		util.pixmap(self.label_cover, color=Qt.GlobalColor.transparent)
-		util.pixmap(self.label_canvas, color=Qt.GlobalColor.transparent)
+		util.pixmap(self.label_cover)
+		util.pixmap(self.label_canvas)
 
 		self.board = np.zeros((9, 9), dtype=np.int8)
-		self.selection = 0, 0
+		self.selection = (0, 0)
 		self.select()
 
 	def press_mouse(self, event):
-		pos = event.pos().y() // self.SIZE, event.pos().x() // self.SIZE
-		self.selection = tuple(np.clip(pos, 0, 8))
+		self.selection = event.pos().y() // self.SIZE, event.pos().x() // self.SIZE
 		self.select()
 
 	def wheelEvent(self, event):
 		self.board[self.selection] += 1 if event.angleDelta().y() > 0 else -1
 		self.board[self.selection] = np.clip(self.board[self.selection], 0, 9)
-		self.display(self.selection, "black", self.board[self.selection])
+		self.display()
 
 	def keyPressEvent(self, event):
 		if event.key() in self.KEY:
 			self.board[self.selection] = self.KEY[event.key()]
-			self.display(self.selection, "black", self.board[self.selection])
-			return
+			self.display()
 		if event.key() == Qt.Key.Key_Backspace:
 			self.board[self.selection] = 0
-			self.display(self.selection, "black", self.board[self.selection])
-			return
+			self.display()
 		if event.key() == Qt.Key.Key_Delete:
 			self.board.fill(0)
 			pixmap = self.label_canvas.pixmap()
 			pixmap.fill(Qt.GlobalColor.transparent)
 			self.label_canvas.setPixmap(pixmap)
-			return
 		if event.key() == Qt.Key.Key_Return:
 			result = DancingLinksAlgorithm.solve(self.board)
 			if isinstance(result, str):
 				util.dialog(result, "error")
 			else:
 				for pos in np.argwhere(self.board != 0):
-					self.display(pos, "black", self.board[tuple(pos)])
+					self.display(pos=pos)
 				for pos in np.argwhere(self.board == 0):
-					self.display(pos, "gray", result[tuple(pos)])
-			return
+					self.display(pos=pos, result=result)
 
-	def display(self, pos, color, tile):
-		xy = pos[1] * self.SIZE, pos[0] * self.SIZE
-		zw = self.SIZE, self.SIZE
+	def display(self, pos=None, result=None):
+		pos = self.selection if pos is None else tuple(pos)
+		tile = self.board[pos] if result is None else result[pos]
 
 		pixmap = self.label_canvas.pixmap()
 		with QPainter(pixmap) as painter:
 			painter.setCompositionMode(QPainter.CompositionMode.CompositionMode_Clear)
-			painter.fillRect(*xy, *zw, Qt.GlobalColor.transparent)
+			painter.fillRect(*self.POSITION[pos], self.SIZE, self.SIZE, Qt.GlobalColor.transparent)
 			if tile != 0:
 				painter.setCompositionMode(QPainter.CompositionMode.CompositionMode_SourceOver)
-				painter.drawImage(*xy, self.IMG[color][tile - 1])
+				painter.drawImage(*self.POSITION[pos], self.IMG["black" if result is None else "gray"][tile - 1])
 		self.label_canvas.setPixmap(pixmap)
 
 	def select(self):
 		pixmap = self.label_cover.pixmap()
 		pixmap.fill(Qt.GlobalColor.transparent)
 		with QPainter(pixmap) as painter:
-			painter.drawImage(self.selection[1] * self.SIZE, self.selection[0] * self.SIZE, self.IMG["selection"])
+			painter.drawImage(*self.POSITION[self.selection], self.IMG["selection"])
 		self.label_cover.setPixmap(pixmap)
 
 
@@ -106,7 +100,7 @@ class DancingLinksAlgorithm:
 	@staticmethod
 	def to_graph(board):
 		graph = CACHE["graph"].copy()
-		for (i, j) in np.argwhere(board != -1):
+		for i, j in np.argwhere(board != -1):
 			r, k = i * 81 + j * 9, board[i, j]
 			graph[r:r + k, :].fill(0)
 			graph[r + k + 1:r + 9, :].fill(0)
@@ -114,13 +108,13 @@ class DancingLinksAlgorithm:
 
 	@staticmethod
 	def to_links(graph):
-		links = tuple(DancingLinksAlgorithm.Node(-1, c) for c in range(graph.shape[1]))
+		links = [DancingLinksAlgorithm.Node(-1, c) for c in range(graph.shape[1])]
 		counts = defaultdict(int)
 		for c, link in enumerate(links):
 			link.left, link.right = links[c - 1], links[c + 1 - len(links)]
 			link.left.right = link.right.left = link
 		for r in range(graph.shape[0]):
-			nodes = tuple(DancingLinksAlgorithm.Node(r, c) for c in np.where(graph[r] == 1)[0])
+			nodes = [DancingLinksAlgorithm.Node(r, c) for c in np.where(graph[r] == 1)[0]]
 			for i, node in enumerate(nodes):
 				counts[node.c] += 1
 				node.up, node.down = links[node.c].up, links[node.c]
