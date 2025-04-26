f91d498f40d9990d074ad3ac6c1f418f55ad8fa9
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-04-26 21:59:24 +0800

    Update greedysnake.py

--- src/greedysnake.py
+++ src/greedysnake.py
@@ -1,108 +1,106 @@
-from greedy_snake_ui import Ui_MainWindow
-from PyQt6.QtCore import QRect, QTimer, Qt
-from PyQt6.QtGui import QPainter, QPixmap
+from greedysnake_ui import Ui_MainWindow
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPainter, QColor, QFont
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from itertools import product
 import random
 import sys
 import util
 
-ROW_COUNT, COL_COUNT, BLOCK_SIZE = 30, 30, 20
-PIXMAP_SIZE = COL_COUNT * BLOCK_SIZE, ROW_COUNT * BLOCK_SIZE
-WINDOW_SIZE = PIXMAP_SIZE[0] + 18, PIXMAP_SIZE[1] + 20
-DIRECTION = {Qt.Key.Key_Up: (-1, 0), Qt.Key.Key_Down: (1, 0), Qt.Key.Key_Left: (0, -1), Qt.Key.Key_Right: (0, 1)}
-
 
 class MyCore(QMainWindow, Ui_MainWindow):
-	direction, field, score = None, None, None
-	snake, food, cut = None, None, None
+	WILDERNESS = {(i, j) for i, j in product(range(20), repeat=2)}
+	POSITION = {(i, j): (j * 25 + 50, i * 25 + 50, 25, 25) for i, j in product(range(20), repeat=2)}
+	DIRECTION = {Qt.Key.Key_Up: (-1, 0), Qt.Key.Key_Down: (1, 0), Qt.Key.Key_Left: (0, -1), Qt.Key.Key_Right: (0, 1)}
+	LARVA = {d: [[(x - i * d[0], y - i * d[1]) for i in range(3)] for x, y in product([9, 10], repeat=2)] for d in DIRECTION.values()}
 
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
-		self.setWindowIcon(util.icon("../greedy_snake/snake"))
+		self.setWindowIcon(util.icon("../greedysnake/logo"))
 
-		self.timer = QTimer()
-		self.timer.setInterval(200)
-		util.cast(self.timer).timeout.connect(self.timeout)
-		
-		self.label.setPixmap(QPixmap(*PIXMAP_SIZE))
+		self.label_theme1 = util.mask(self.label_canvas, (9, 0), "../greedysnake/theme1")
+		self.label_theme2 = util.mask(self.label_canvas, (9, 0), "../greedysnake/theme2", hide=True)
+		self.label_scoreboard = util.mask(self.label_canvas, (9, 0), "../greedysnake/scoreboard")
+		self.label_pause = util.mask(self.label_canvas, (9, 0), QColor(255, 255, 255, 80), hide=True)
+		util.pixmap(self.label_canvas, color=Qt.GlobalColor.black)
+
+		self.direction = self.snake = self.food = None
+		self.timer = util.timer(200, self.timeout)
 		self.restart()
 
 	def restart(self):
-		self.score = 0
-		self.direction = random.choice(list(DIRECTION.values()))
-		self.snake = [(ROW_COUNT // 2 + i * self.direction[0], COL_COUNT // 2 + i * self.direction[1]) for i in range(3)]
-		self.field = [(i, j) for i, j in product(range(ROW_COUNT), range(COL_COUNT)) if (i, j) not in self.snake]
-		self.food = random.choice(self.field)
-		self.draw()
+		self.direction = random.choice(list(self.DIRECTION.values()))
+		self.snake = random.choice(self.LARVA[self.direction]).copy()
+		self.food = random.choice(list(self.WILDERNESS - set(self.snake)))
+
+		self.timer.start()
+		self.timeout()
+
+	def mousePressEvent(self, a0):
+		if a0.button() == Qt.MouseButton.RightButton:
+			self.label_theme1.setHidden(not self.label_theme1.isHidden())
+			self.label_theme2.setHidden(not self.label_theme2.isHidden())
+
+	def keyPressEvent(self, event):
+		if event.key() == Qt.Key.Key_Space:
+			if self.timer.isActive():
+				self.timer.stop()
+				return self.label_pause.show()
+			else:
+				self.timer.start()
+				return self.label_pause.hide()
+
+		if not self.timer.isActive():
+			return
+		if event.key() == Qt.Key.Key_Return:
+			self.timer.stop()
+			return self.restart()
+
+		if event.key() in self.DIRECTION:
+			direction = self.DIRECTION[event.key()]
+			if self.direction[0] != direction[0] and self.direction[1] != direction[1]:
+				self.direction = direction
+				self.timer.start()
+				self.timeout()
 
 	def timeout(self):
-		head = self.snake[-1][0] + self.direction[0], self.snake[-1][1] + self.direction[1]
-		if head in self.snake or not 0 <= head[0] < ROW_COUNT or not 0 <= head[1] < COL_COUNT:
+		head = self.snake[0][0] + self.direction[0], self.snake[0][1] + self.direction[1]
+		if head in self.snake or not 0 <= head[0] < 20 or not 0 <= head[1] < 20:
 			util.dialog("Game over", "error")
 			return self.restart()
 
-		self.snake.append(head)
-		self.field.remove(head)
+		self.snake.insert(0, head)
 		if head == self.food:
-			self.score += 1
-			if random.random() < 0.2 and len(self.snake) >= 5 and not self.cut:
-				self.food, self.cut = random.sample(self.field, 2)
-			else:
-				self.food = random.choice(self.field)
-		elif head == self.cut:
-			self.score += 2
-			self.cut = None
-			for _ in range(3):
-				self.field.append(self.snake.pop(0))
+			self.food = random.choice(list(self.WILDERNESS - set(self.snake)))
+			self.scoring()
 		else:
-			self.field.append(self.snake.pop(0))
-		self.draw()
+			self.snake.pop()
+		self.display()
 
-	def keyPressEvent(self, a0):
-		if not self.timer.isActive():
-			return
-		if a0.key() not in DIRECTION:
-			return
-
-		direct = DIRECTION[util.cast(a0.key())]
-		if self.direction[0] != direct[0] and self.direction[1] != direct[1]:
-			self.direction = direct
-			self.timeout()
-			self.timer.start()
-
-	def draw(self):
-		pixmap = self.label.pixmap()
+	def display(self):
+		pixmap = self.label_canvas.pixmap()
 		pixmap.fill(Qt.GlobalColor.black)
 		with QPainter(pixmap) as painter:
-			painter.setPen(Qt.PenStyle.NoPen)
-			if self.cut:
-				painter.setBrush(Qt.GlobalColor.blue)
-				painter.drawRect(QRect(self.cut[1] * BLOCK_SIZE, self.cut[0] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-			if self.food:
-				painter.setBrush(Qt.GlobalColor.red)
-				painter.drawRect(QRect(self.food[1] * BLOCK_SIZE, self.food[0] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-			for s in self.snake:
-				painter.setBrush(Qt.GlobalColor.white)
-				painter.drawRect(QRect(s[1] * BLOCK_SIZE, s[0] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-		self.label.setPixmap(pixmap)
-		self.statusbar.showMessage(f"当前得分: {self.score}")
+			painter.fillRect(*self.POSITION[self.food], Qt.GlobalColor.red)
+			offset = int(200 / len(self.snake))
+			for i, pos in enumerate(self.snake):
+				painter.fillRect(*self.POSITION[pos], QColor(0, 255, 0, 255 - offset * i))
+		self.label_canvas.setPixmap(pixmap)
 
-	def leaveEvent(self, a0):
-		super().leaveEvent(a0)
-		if self.timer.isActive():
-			self.timer.stop()
-
-	def enterEvent(self, event):
-		super().enterEvent(event)
-		if not self.timer.isActive():
-			self.timer.start()
+	def scoring(self):
+		pixmap = self.label_scoreboard.pixmap()
+		pixmap.fill(Qt.GlobalColor.transparent)
+		with QPainter(pixmap) as painter:
+			painter.setFont(QFont("", 16))
+			painter.setPen(Qt.GlobalColor.white)
+			painter.drawText(60, 60, 100, 50, Qt.AlignmentFlag.AlignLeft, f"Score {len(self.snake) - 3}")
+		self.label_scoreboard.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	my_core = MyCore()
-	my_core.setFixedSize(*WINDOW_SIZE)
+	my_core.setFixedSize(my_core.window().size())
 	my_core.show()
 	sys.exit(app.exec())
